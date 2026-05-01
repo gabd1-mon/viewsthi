1e94be9585c99388dd1123a68e4910773e79a51f
Gabriel Davi <gabriel16monteiro@hotmail.com>
2026-05-01 13:07:59 -0300

    Primeiras adições: SideBar

---
+++ resources/js/Pages/Dashboard.jsx
@@ -0,0 +1,10 @@
+import PanelLayout from '@/Layouts/PanelLayout';
+
+export default function Dashboard() {
+    return (
+        <PanelLayout>
+            <h1 className="text-2xl font-bold">Dashboard</h1>
+            <p className="mt-2">R$ 1.250,00</p>
+        </PanelLayout>
+    );
+}
